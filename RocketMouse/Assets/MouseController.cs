2fba9d219b8343dee1d4d74a84946e02c27c2897
usmansheik <12105089@gift.edu.pk>
2016-10-29 19:13:52 +0500

    Half Game complete

---
+++ RocketMouse/Assets/MouseController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+    public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
+    // Use this for initialization
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+
+        if (jetpackActive)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
+        }
+        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+        newVelocity.x = forwardMovementSpeed;
+        GetComponent<Rigidbody2D>().velocity = newVelocity;
+    }
+}
