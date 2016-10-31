a01c020377cca3d5fbdf0e89263cd2be5c106ddf
usmansheik <12105089@gift.edu.pk>
2016-10-31 19:04:41 +0500

    1/4 part of 3rd Tutorial Complete

---
+++ RocketMouse/Assets/scripts/MouseController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+    public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
+    private uint coins = 0;
+    public Transform groundCheckTransform;
+    public ParticleSystem jetpack;
+    private bool grounded;
+    private bool dead = false;
+    public LayerMask groundCheckLayerMask;
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void UpdateGroundedStatus()
+    {
+        //1
+        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        Debug.Log(grounded);
+        //2
+        animator.SetBool("grounded", grounded);
+    }
+    void AdjustJetpack(bool jetpackActive)
+    {
+        jetpack.enableEmission = !grounded;
+        jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
+    }
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+
+        if (jetpackActive)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
+        }
+        if (!dead)
+        {
+            Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+            newVelocity.x = forwardMovementSpeed;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+        UpdateGroundedStatus();
+        AdjustJetpack(jetpackActive);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Coins"))
+            CollectCoin(collider);
+        else
+            HitByLaser(collider);
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+    animator.SetBool("dead", true);
+        forwardMovementSpeed = 0;
+    }
+    void CollectCoin(Collider2D coinCollider)
+    {
+        coins++;
+
+        Destroy(coinCollider.gameObject);
+    }
+}
